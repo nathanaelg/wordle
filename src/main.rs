7a8f104da90806f968ef8cb913937cb6807d022f
Nathanael Gray <nathanaelg@gmail.com>
2022-01-08 20:28:21 +1300

    Evaluate guess and implement adding of guess evaluation.

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,83 @@
 use std::collections::HashSet;
+use std::ops;
 
-fn evaluate_guess(guess: &Vec<char>, correct: &Vec<char>) -> Vec<char>
-{
-    let mut greens = vec!['\0'; 5];
+struct Guess {
+    green: Vec<char>,
+    yellow: Vec<HashSet<char>>,
+    grey: HashSet<char>,
+}
 
-    let it = correct.iter().zip(guess.iter());
+fn evaluate_guess(guess: Vec<char>, correct: Vec<char>) -> Guess {
+    let green = correct
+        .iter()
+        .zip(guess.iter())
+        .map(|(&c, &g)| if c == g { c } else { '\0' })
+        .collect();
 
-    for (i, (c, g)) in it.enumerate() {
-        if *c == *g
-        {
-            greens[i] = *c;
-        }
+    let correct_set: HashSet<char> = HashSet::from_iter(correct.iter().cloned());
+
+    let yellow = correct
+        .iter()
+        .zip(guess.iter())
+        .map(|(&c, &g)| if c != g && correct_set.contains(&g) { HashSet::from([g]) } else { HashSet::new() })
+        .collect();
+
+    let guess_set: HashSet<char> = HashSet::from_iter(guess.iter().cloned());
+    let grey = &guess_set - &correct_set;
+
+    return Guess { green, yellow, grey };
+}
+
+fn possible_word(word: Vec<char>, guess: Guess) -> bool {
+    return false;
+}
+
+impl ops::Add<Guess> for Guess {
+    type Output = Guess;
+
+    fn add(self, _rhs: Guess) -> Guess {
+        let green = self.green
+        .iter()
+        .zip(_rhs.green.iter())
+        .map(|(&g1, &g2)| if g1 == '\0' { g2 } else { g1 })
+        .collect();
+
+        let yellow = self.yellow
+        .into_iter()
+        .zip(_rhs.yellow.into_iter())
+        .map(|(y1, y2)| y1.into_iter().chain(y2).collect())
+        .collect();
+
+        let grey = self.grey.into_iter().chain(_rhs.grey).collect();
+
+        return Guess{green, yellow, grey};
     }
-    return greens;
 }
 
 fn main() {
-    let mut greens = vec!['\0'; 5];
-    let mut yellows = vec![HashSet::with_capacity(10); 5];
-    let mut greys = HashSet::with_capacity(26);
+    let correct = String::from("slump");
+    let guess1 = String::from("raise");
+    let guess2 = String::from("snout");
+    let guess3 = String::from("sully");
+    let guess4 = String::from("slump");
 
-    yellows[0].insert('i');
-    yellows[1].insert('r');
-    greys.insert('a');
-    yellows[3].insert('t');
-    yellows[4].insert('e');
+    let mut guess_eval = evaluate_guess(guess1.chars().collect(), correct.chars().collect());
+    println!("Green: {:?}", guess_eval.green);
+    println!("Yellow: {:?}", guess_eval.yellow);
+    println!("Grey: {:?}", guess_eval.grey);
 
-    println!("Green: {:?}", greens);
-    println!("Yellow: {:?}", yellows);
-    println!("Grey: {:?}", greys);
+    guess_eval = guess_eval + evaluate_guess(guess2.chars().collect(), correct.chars().collect());
+    println!("Green: {:?}", guess_eval.green);
+    println!("Yellow: {:?}", guess_eval.yellow);
+    println!("Grey: {:?}", guess_eval.grey);
 
-    greens[0] = 't';
-    greens[1] = 'i';
-    yellows[1].insert('r');
-    greens[3] = 'e';
-    greys.insert('d');
+    guess_eval = guess_eval + evaluate_guess(guess3.chars().collect(), correct.chars().collect());
+    println!("Green: {:?}", guess_eval.green);
+    println!("Yellow: {:?}", guess_eval.yellow);
+    println!("Grey: {:?}", guess_eval.grey);
 
-    println!("Green: {:?}", greens);
-    println!("Yellow: {:?}", yellows);
-    println!("Grey: {:?}", greys);
-
-    greens[2] = 'g';
-    greens[4] = 'r';
-
-    println!("Green: {:?}", greens);
-    println!("Yellow: {:?}", yellows);
-    println!("Grey: {:?}", greys);
-
-    let correct = String::from("banal");
-    let guess = String::from("candy");
-
-    greens = evaluate_guess(&guess.chars().collect(), &correct.chars().collect());
-
-    println!("Green: {:?}", greens);
-    println!("Yellow: {:?}", yellows);
-    println!("Grey: {:?}", greys);
+    guess_eval = guess_eval + evaluate_guess(guess4.chars().collect(), correct.chars().collect());
+    println!("Green: {:?}", guess_eval.green);
+    println!("Yellow: {:?}", guess_eval.yellow);
+    println!("Grey: {:?}", guess_eval.grey);
 }
